c028f3c91ec7e63f47082813333a64c0442ed582
tbudin <thomas@wiridis.com>
2021-11-05 21:40:14 +0800

    Hero section v1

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,6 +13,9 @@ export default function Layout({ children }: Props) {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#fff" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500&family=Monda:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
       <nav>
         <Navigation />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
 import TwitterCardMeta from "../components/meta/TwitterCardMeta";
-import { SocialList } from "../components/SocialList";
 
 export default function Index() {
   return (
@@ -10,51 +9,113 @@ export default function Index() {
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
-      <div className="container">
-        <div>
-          <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
-          </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
-          <SocialList />
-        </div>
-      </div>
+
+      <main class="main" data-scroll-container="">
+        <section class="hero" data-scroll-section="">
+          <div class="logo-wrap">
+            <div class="fire"></div>
+            <hr class="hero-line" data-scroll="" data-scroll-class="is-scaled"/>
+            <div class="logo-mark" data-scroll="" data-scroll-class="appear">
+              <svg viewbox="0 0 160 99" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0V49.3641L64.0378 98.7283V49.3641L0 0Z" fill="#FFFDFB"></path>
+                <path d="M64.0372 0V49.3641L128.075 98.7283V49.3641L64.0372 0Z" fill="#FFFDFB"></path>
+                <path d="M128.076 0V31.0007L159.866 55.6827L159.94 24.6821L128.076 0Z" fill="#FFFDFB"></path>
+              </svg>
+              <h1 class="logo-type" data-scroll="" data-scroll-class="appear">wiridis</h1>
+            </div>
+            <hr class="hero-line" data-scroll="" data-scroll-class="is-scaled"/>
+          </div>
+        </section>
+      </main>
       <style jsx>{`
-        .container {
+        .hero {
+          min-height: 100vh;
           display: flex;
+          flex-direction: column;
           align-items: center;
           justify-content: center;
-          flex: 1 1 auto;
-          padding: 0 1.5rem;
-        }
-        h1 {
-          font-size: 2.5rem;
-          margin: 0;
-          font-weight: 500;
-        }
-        h2 {
-          font-size: 1.75rem;
-          font-weight: 400;
-          line-height: 1.25;
-        }
-        .fancy {
-          color: #15847d;
-        }
-        .handle {
-          display: inline-block;
-          margin-top: 0.275em;
-          color: #9b9b9b;
-          letter-spacing: 0.05em;
+          text-align: center;
         }
 
-        @media (min-width: 769px) {
-          h1 {
-            font-size: 3rem;
-          }
-          h2 {
-            font-size: 2.25rem;
-          }
+        .hero-line {
+          border: 0;
+          position: relative;
+          height: 40vh;
+          width: 2px;
+          margin: 0 auto;
+          background: rgba(255, 253, 251, 0.4);
+        }
+
+        .logo-wrap {
+          position: relative;
+          width: 100%;
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: space-between;
+        }
+
+        .fire {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          width: 70vw;
+          height: 70vw;
+          min-width: 720px;
+          min-height: 720px;
+          border-radius: 50%;
+          background-color: #7A3542;
+          filter: blur(50px);
+          transform: translate(-50%, -75%);
+        }
+
+        .fire::after {
+          content: "";
+          position: absolute;
+          background: linear-gradient(rgba(233, 112, 65, 0), #FED29D 90%);
+          bottom: 0;
+          left: 0;
+          height: 90%;
+          width: 90%;
+          transform: translateX(5%);
+          border-radius: 50%;
+        }
+
+        .logo-type {
+          position: relative;
+          font-size: 2rem;
+          letter-spacing: 0.4rem;
+        }
+
+        .logo-mark {
+          position: relative;
+        }
+        .logo-mark svg {
+          display: block;
+          width: 100px;
+          margin: 0 auto;
+        }
+
+        .logo-type,
+        .subtitle {
+          font-family: "Monda", sans-serif;
+          text-transform: uppercase;
+          line-height: 1.1;
+        }
+
+        .subtitle {
+          font-weight: 700;
+          font-size: 1.5rem;
+          margin-bottom: 1.5rem;
+          letter-spacing: 0.2rem;
+        }
+
+        .large-text {
+          font-family: "Inter", sans-serif;
+          font-weight: 200;
+          line-height: 1.1;
+          opacity: 0.8;
         }
       `}</style>
     </Layout>
